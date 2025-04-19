459ab39f758d1c1e9a90b64f39652c117f3c8f30
Boyu Zhang <boyu.zhang@tum.de>
2025-04-19 22:26:06 +0100

    yolov8

---
+++ src/yolo2ros_bridge/yolo_segmentation/scripts/test_yolo.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from ultralytics import YOLO
+import os
+
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(current_dir, "../../YOLO_models/yolov8l-pose.pt")  
+model_path = os.path.abspath(model_path)
+model = YOLO(model_path)
+model.to('cuda:0')
+
+file_path = os.path.join(current_dir, "../../../test/test_images/workers.jpg")
+file_path = os.path.join(current_dir, "../../../test/test_videos/test.mp4")
+file_path = os.path.abspath(file_path)
+
+
+# Run inference with the YOLO12n model on the 'bus.jpg' image
+results = model(file_path, save=True, save_txt=True, show=True)
+
+
